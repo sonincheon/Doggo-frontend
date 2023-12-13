6bb43890ff20bba3ddbd74e38769955ae75f445f
lumiereJE <lumiere_remplir@naver.com>
2023-12-13 10:15:26 +0900

    2023/12/13 김지은 app 수정

--- src/App.js
+++ src/App.js
@@ -63,13 +63,13 @@ function App() {
               <Route path="/mypage" element={<MyPage />} />
             </Route>
           {/* 여긴 어드민 구간  */}
-          <Route element={<Admin />}>
-            <Route path="/admin" element={<Adminmainpage/>} />
-            <Route path="/admin/member" element={<Adminmemberpage/>} />
-            <Route path="/admin/qna" element={<Adminqnapage/>} />
-            <Route path="/admin/sales" element={<Adminsalespage/>} />
-            <Route path="/admin/qna/detail" element={<Qnadetailpage/>} />
-          </Route>
+            <Route element={<Admin/>}>
+              <Route path="/admin" element={<Adminmainpage/>}/>
+              <Route path="/admin/user" element={<Adminmemberpage/>}/>
+              <Route path="/admin/sales" element={<Adminsalespage/>}/>
+              <Route path="/admin/qna" element={<Adminqnapage/>}/>
+              <Route path="/admin/qna/:id" element={<Qnadetailpage/>}/>
+            </Route>
         </Routes>
       </Router>
     </>
