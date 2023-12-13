6f12ef8ba12c44608f43e8f60f562652a7fc3d82
PANG2OPPA <khbkhb85098509@gmail.com>
2023-12-13 21:02:19 +0900

    2023.12.13 김현빈 마이페이지 출력 정보 추가, 페이지 크기 조정

--- src/components/member/Myprofile.jsx
+++ src/components/member/Myprofile.jsx
@@ -5,31 +5,29 @@ import Usermodal from "../../utill/Usermodal";
 import Pwdmodal from "../../utill/Pwdmodal";
 import Imgmodal from "../../utill/Imgmodal";
 
-
-
 const BoxContent = styled.div`
-    width: 400px;
-    height: 800px;
-    background-color: #D9D9D9;
-    border-radius: 10px;
+  width: 400px;
+  height: 110vh;
+  background-color: #d9d9d9;
+  border-radius: 10px;
 `;
 
 const BoxTitle = styled.div`
-    font-size: 45px;
-    margin-bottom: 1rem;
+  font-size: 45px;
+  margin-bottom: 1rem;
 `;
 
 const BoxContent1 = styled.div`
-    height: 350px;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  height: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 const BoxContent2 = styled.div`
-    height: 410px;
-    margin-top: 10px;
+  height: 410px;
+  margin-top: 10px;
 `;
 
 // const Profile = styled.img`
@@ -46,228 +44,268 @@ const BoxContent2 = styled.div`
 // `;
 
 const Btn = styled.button`
-    width: 75px;
-    height: 30px;
-    border-radius: 10px;
-    font-size: 15px;
-    font-weight: bold;
-    color: white;
-    background-color: #776B5D;
-    box-sizing: border-box;
-    vertical-align: bottom;
-    margin-right: 5px;
+  width: 75px;
+  height: 30px;
+  border-radius: 10px;
+  font-size: 15px;
+  font-weight: bold;
+  color: white;
+  background-color: #776b5d;
+  box-sizing: border-box;
+  vertical-align: bottom;
+  margin-right: 5px;
 `;
 
 const InputBox = styled.div`
-    width: 400px;
-    height: 50px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    font-size: 14px;
-    font-weight: bold;
-    justify-content: space-between;
-    padding-left: 5px;
-    margin-bottom: 1rem;
+  width: 400px;
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: bold;
+  justify-content: space-between;
+  padding-left: 5px;
+  margin-bottom: 1rem;
 `;
 
 const Input = styled.div`
-    width: 240px;
-    height: 40px;
-    border-radius: 10px;
-    margin-right: 5px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 15px;
+  width: 240px;
+  height: 40px;
+  border-radius: 10px;
+  margin-right: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 15px;
 `;
 
 const Btn1 = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 190px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 190px;
 `;
 
 const Btn2 = styled.button`
-    width: 300px;
-    height: 40px;
-    border-radius: 10px;
-    font-size: 17px;
-    font-weight: bold;
-    color: white;
-    background-color: #776B5D;
-    box-sizing: border-box;
-    vertical-align: bottom;
-    margin-top: 8px;
+  width: 300px;
+  height: 40px;
+  border-radius: 10px;
+  font-size: 17px;
+  font-weight: bold;
+  color: white;
+  background-color: #776b5d;
+  box-sizing: border-box;
+  vertical-align: bottom;
+  margin-top: 8px;
 `;
 
 const Profile = styled.div`
-     position: relative;
-    display: inline-block;
-    overflow: hidden;
-    height: 250px;
-    width: 250px;
-    border-radius: 100%;
-    background-color: #ffe;
-    margin-bottom: 1rem;
-    
+  position: relative;
+  display: inline-block;
+  overflow: hidden;
+  height: 250px;
+  width: 250px;
+  border-radius: 100%;
+  background-color: #ffe;
+  margin-bottom: 1rem;
 `;
 
 const ProfileImage = styled.img`
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    transition: filter 0.3s ease; /* 부드러운 효과를 위한 트랜지션 속성 추가 */
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  transition: filter 0.3s ease; /* 부드러운 효과를 위한 트랜지션 속성 추가 */
 
-    ${Profile}:hover & {
-        filter: grayscale(100%) blur(5px); /* 마우스 호버 시 이미지에 효과 적용 */
-    }
+  ${Profile}:hover & {
+    filter: grayscale(100%) blur(5px); /* 마우스 호버 시 이미지에 효과 적용 */
+  }
 `;
 
 const OverlayText = styled.div`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    color: white; /* 텍스트 색상 설정 */
-    font-size: 25px; /* 텍스트 크기 설정 */
-    font-weight: bold; /* 텍스트 굵기 설정 */
-    pointer-events: none; /* 텍스트 위에 마우스 이벤트를 허용하지 않음 */
-    opacity: 0; /* 초기에는 보이지 않도록 설정 */
-    transition: opacity 0.3s ease; /* 투명도에 대한 부드러운 효과를 위한 트랜지션 속성 추가 */
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: white; /* 텍스트 색상 설정 */
+  font-size: 25px; /* 텍스트 크기 설정 */
+  font-weight: bold; /* 텍스트 굵기 설정 */
+  pointer-events: none; /* 텍스트 위에 마우스 이벤트를 허용하지 않음 */
+  opacity: 0; /* 초기에는 보이지 않도록 설정 */
+  transition: opacity 0.3s ease; /* 투명도에 대한 부드러운 효과를 위한 트랜지션 속성 추가 */
 
-    ${Profile}:hover & {
-        opacity: 1; /* 마우스 호버 시 텍스트가 나타남 */
-    }
+  ${Profile}:hover & {
+    opacity: 1; /* 마우스 호버 시 텍스트가 나타남 */
+  }
 `;
 
-
-
-
 const Myprofile = () => {
-    const [modalOpen, setModalOpen] = useState(false);
-    const [headerName, setHeaderName] = useState("닉네임 변경");
-    const [bodyName, setBodyName] = useState("닉네임");
-    const [bodyContent, setBodyContent] = useState("");
-    const [modalOpen1, setModalOpen1] = useState(false);
-    const [modalOpen2, setModalOpen2] = useState(false);
-    const [password, setPassword] = useState("");
-    const [image, setImage] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [headerName, setHeaderName] = useState("닉네임 변경");
+  const [bodyName, setBodyName] = useState("닉네임");
+  const [bodyContent, setBodyContent] = useState("");
+  const [modalOpen1, setModalOpen1] = useState(false);
+  const [modalOpen2, setModalOpen2] = useState(false);
+  const [password, setPassword] = useState("");
+  const [image, setImage] = useState("");
 
-    const closeModal = () => {
-      setModalOpen(false);
-    };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
-    const closeModal1 = () => {
-        setModalOpen1(false);
-      };
+  const closeModal1 = () => {
+    setModalOpen1(false);
+  };
 
-    const closeModal2 = () => {
-        setModalOpen2(false);
-    };
+  const closeModal2 = () => {
+    setModalOpen2(false);
+  };
 
-    const openClick =(Name, content, bodyConetent)=>{
-        setHeaderName(Name);
-        setModalOpen(true);
-        setBodyName(content);
-        setBodyContent(bodyConetent);
-    };
+  const openClick = (Name, content, bodyConetent) => {
+    setHeaderName(Name);
+    setModalOpen(true);
+    setBodyName(content);
+    setBodyContent(bodyConetent);
+  };
 
-    const openClick1 = (Password) => {
-        setModalOpen1(true);
-        setPassword(Password);
-    };
+  const openClick1 = (Password) => {
+    setModalOpen1(true);
+    setPassword(Password);
+  };
 
-    const openClick2 = (img) => {
-        setModalOpen2(true);
-        setImage(img);
-    }
+  const openClick2 = (img) => {
+    setModalOpen2(true);
+    setImage(img);
+  };
 
-    
-    const member = [
-        {
-            img : "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KDcViVN5lc9nXMk9kayDMM1E4MLfKV2ouBZdod6Drpsz8X6V71hi0io22P5eH8G_SoIbmtVgh_5pl3x2xzDaXQq_cCG0bNovXTCRUs9Hxico_Tx7chlHEVhTc0_GWP8hK-7-dQjGJJLX4qdjzLVxZw.jpg?alt=media&token=5ac08f40-1599-4a19-b65e-bd5e57b24cb2",
-            Name : "손인천",
-            Email : "in1000s@naver.com",
-            Address : "서울특별시 신림동 신사로 12길 32",
-            Tel : "010-9118-4893", 
-            Password : "son12345"  
-        }
-    ]
+  const member = [
+    {
+      img: "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KDcViVN5lc9nXMk9kayDMM1E4MLfKV2ouBZdod6Drpsz8X6V71hi0io22P5eH8G_SoIbmtVgh_5pl3x2xzDaXQq_cCG0bNovXTCRUs9Hxico_Tx7chlHEVhTc0_GWP8hK-7-dQjGJJLX4qdjzLVxZw.jpg?alt=media&token=5ac08f40-1599-4a19-b65e-bd5e57b24cb2",
+      Name: "손인천",
+      Email: "in1000s@naver.com",
+      Address: "서울특별시 신림동 신사로 12길 32",
+      Gender: "남",
+      Birth: "2000-02-02",
+      Tel: "010-9118-4893",
+      Password: "son12345",
+    },
+  ];
 
   return (
-        <div>
-            <BoxTitle>MY PROFILE</BoxTitle>
-            <BoxContent>
-                <BoxContent1>
-                    <Profile>
-                        <ProfileImage src={member[0].img} onClick={()=>openClick2(member[0].img)}/>
-                        <OverlayText>수정</OverlayText>
-                    </Profile>
-                </BoxContent1>
-                    <div style={{textAlign:'center', fontSize:'25px', fontWeight:'bold', marginBottom:'1rem'}}>{member[0].Name}님</div>
-                <BoxContent2>
-                    <InputBox>
-                        이메일 :
-                        <div style={{display:'flex'}}>
-                            <Input>
-                                {member[0].Email}
-                            </Input> 
-                                <Btn onClick={()=>openClick("이메일 변경", "이메일", member[0].Email)}>
-                                    수정
-                                </Btn>
-                        </div>
-                    </InputBox>
-                    <InputBox>
-                        주소 :
-                        <div style={{display:'flex'}}>
-                            <Input>
-                                {member[0].Address}
-                            </Input> 
-                                <Btn onClick={()=>openClick("주소 변경", "주소", member[0].Address)}>
-                                    수정
-                                </Btn>
-                        </div>
-                    </InputBox>
-                    <InputBox>
-                        전화번호 :
-                        <div style={{display:'flex'}}>
-                            <Input>
-                                {member[0].Tel}
-                            </Input> 
-                                <Btn onClick={()=>openClick("전화번호 변경", "전화번호", member[0].Tel)}>
-                                    수정
-                                </Btn>
-                        </div>
-                    </InputBox>
-                    <Btn1>
-                        <Btn2 onClick={() =>openClick1(member[0].Password)}>
-                            비밀번호 변경
-                        </Btn2>
-                        <Btn2>
-                            1대1 문의하기
-                        </Btn2>
-                        <Btn2>
-                            구매 내역 조회
-                        </Btn2>
-                    </Btn1>
-                </BoxContent2>
-            </BoxContent>
-            <Usermodal type={1} open={modalOpen} close={closeModal}  header={headerName} name={bodyName} detail={bodyContent} >
-            </Usermodal>
-            <Pwdmodal type={1} open={modalOpen1} close={closeModal1} detail={password}>
-            </Pwdmodal>
-            <Imgmodal type={1} open={modalOpen2} close={closeModal2}  header={headerName} name={bodyName} image={image}>
-
-            </Imgmodal>
-
-
-
+    <div>
+      <BoxTitle>MY PROFILE</BoxTitle>
+      <BoxContent>
+        <BoxContent1>
+          <Profile>
+            <ProfileImage
+              src={member[0].img}
+              onClick={() => openClick2(member[0].img)}
+            />
+            <OverlayText>수정</OverlayText>
+          </Profile>
+        </BoxContent1>
+        <div
+          style={{
+            textAlign: "center",
+            fontSize: "25px",
+            fontWeight: "bold",
+            marginBottom: "1rem",
+          }}
+        >
+          {member[0].Name}님
         </div>
-
+        <BoxContent2>
+          <InputBox>
+            이메일 :
+            <div style={{ display: "flex" }}>
+              <Input>{member[0].Email}</Input>
+            </div>
+            <div style={{ width: "75px", height: "30px" }}></div>
+          </InputBox>
+          <InputBox>
+            성별 :
+            <div style={{ display: "flex" }}>
+              <Input>{member[0].Gender}</Input>
+              <Btn
+                onClick={() => openClick("성별 변경", "성별", member[0].Gender)}
+              >
+                수정
+              </Btn>
+            </div>
+          </InputBox>
+          <InputBox>
+            생년월일 :
+            <div style={{ display: "flex" }}>
+              <Input>{member[0].Birth}</Input>
+              <Btn
+                onClick={() =>
+                  openClick("생년월일 변경", "생년월일", member[0].Birth)
+                }
+              >
+                수정
+              </Btn>
+            </div>
+          </InputBox>
+          <InputBox>
+            주소 :
+            <div style={{ display: "flex" }}>
+              <Input>{member[0].Address}</Input>
+              <Btn
+                onClick={() =>
+                  openClick("주소 변경", "주소", member[0].Address)
+                }
+              >
+                수정
+              </Btn>
+            </div>
+          </InputBox>
+          <InputBox>
+            전화번호 :
+            <div style={{ display: "flex" }}>
+              <Input>{member[0].Tel}</Input>
+              <Btn
+                onClick={() =>
+                  openClick("전화번호 변경", "전화번호", member[0].Tel)
+                }
+              >
+                수정
+              </Btn>
+            </div>
+          </InputBox>
+          <Btn1>
+            <Btn2 onClick={() => openClick1(member[0].Password)}>
+              비밀번호 변경
+            </Btn2>
+            <Btn2>1대1 문의하기</Btn2>
+            <Btn2>구매 내역 조회</Btn2>
+          </Btn1>
+        </BoxContent2>
+      </BoxContent>
+      <Usermodal
+        type={1}
+        open={modalOpen}
+        close={closeModal}
+        header={headerName}
+        name={bodyName}
+        detail={bodyContent}
+      ></Usermodal>
+      <Pwdmodal
+        type={1}
+        open={modalOpen1}
+        close={closeModal1}
+        detail={password}
+      ></Pwdmodal>
+      <Imgmodal
+        type={1}
+        open={modalOpen2}
+        close={closeModal2}
+        header={headerName}
+        name={bodyName}
+        image={image}
+      ></Imgmodal>
+    </div>
   );
 };
 export default Myprofile;
--- src/components/member/Petprofile.jsx
+++ src/components/member/Petprofile.jsx
@@ -1,223 +1,249 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styled, { css } from "styled-components";
-import {ReactComponent as Footimg } from "../../img/Group 50.svg"
-import dogfoot from '../../img/dogfoot.png';
+import { ReactComponent as Footimg } from "../../img/Group 50.svg";
+import dogfoot from "../../img/dogfoot.png";
 import Petmodal from "../../utill/Petmodal";
 
 const BoxContent = styled.div`
-    width: 780px;
-    height: 800px;
-    background-color: #D9D9D9;
-    border-radius: 10px;
-    overflow-y: scroll;
-    overflow-x: hidden;
+  width: 780px;
+  height: 110vh;
+  background-color: #d9d9d9;
+  border-radius: 10px;
+  overflow-y: scroll;
+  overflow-x: hidden;
 
-    &::-webkit-scrollbar {
-        width: 13px; // 스크롤바의 너비
-    }
+  &::-webkit-scrollbar {
+    width: 13px; // 스크롤바의 너비
+  }
 
-    &::-webkit-scrollbar-thumb {
-        background-color: #776B5D; // 스크롤바 색상
-        border-radius: 7px; // 스크롤바 모양 (모서리 둥글게)
-    }
+  &::-webkit-scrollbar-thumb {
+    background-color: #776b5d; // 스크롤바 색상
+    border-radius: 7px; // 스크롤바 모양 (모서리 둥글게)
+  }
 
-    &::-webkit-scrollbar-track {
-        background-color: #B0A695; // 스크롤바 색상
-        border-radius: 7px; // 스크롤바 모양 (모서리 둥글게)
-        // F3EEEA, EBE3D5, FFEED9, B0A695
-    }
+  &::-webkit-scrollbar-track {
+    background-color: #b0a695; // 스크롤바 색상
+    border-radius: 7px; // 스크롤바 모양 (모서리 둥글게)
+    // F3EEEA, EBE3D5, FFEED9, B0A695
+  }
 `;
 
 const BoxTitle = styled.div`
-    font-size: 45px;
-    margin-bottom: 1rem;
+  font-size: 45px;
+  margin-bottom: 1rem;
 `;
 
 const BoxContent1 = styled.div`
-    width: 770px;
-    height: 220px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    margin-bottom: 1rem;
-    margin-top: 1rem;
+  width: 770px;
+  height: 220px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: 1rem;
+  margin-top: 1rem;
 `;
 
 const PetProfile = styled.img`
-    width: 220px;
-    height: 220px;
-    border-radius: 100%;
-    background-color: #FFEED9;
-    margin-left: 20px;
+  width: 220px;
+  height: 220px;
+  border-radius: 100%;
+  background-color: #ffeed9;
+  margin-left: 20px;
 `;
 
 const PetInfo1 = styled.div`
-    position: relative;
-    width: 500px;
-    height: 220px;
-    background-color: white;
-    border-radius: 10px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center; 
-    margin-right: 10px;
-    z-index: 1;
+  position: relative;
+  width: 500px;
+  height: 220px;
+  background-color: white;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-right: 10px;
+  z-index: 1;
 
-    .PetSign {
-        font-size: 20px;
-        font-weight: bold;
-        margin-bottom: 1rem;
-        margin-left: 1rem; 
-    }
+  .PetSign {
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 1rem;
+    margin-left: 1rem;
+  }
 
-    .DogFootImage {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
+  .DogFootImage {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
 `;
 
 const PetInfo2 = styled.div`
-    z-index: 1;
-    .PetSign {
-        display: flex;
-    }
+  z-index: 1;
+  .PetSign {
+    display: flex;
+  }
 `;
 
 const PetInfo3 = styled.div`
-    height: 207px;
-
+  height: 207px;
 `;
 
 const Btn = styled.button`
-    width: 75px;
-    height: 30px;
-    border-radius: 10px;
-    font-size: 15px;
-    font-weight: bold;
-    color: white;
-    background-color: #776B5D;
-    box-sizing: border-box;
-    vertical-align: bottom;
-    margin-right: 5px;
+  width: 75px;
+  height: 30px;
+  border-radius: 10px;
+  font-size: 15px;
+  font-weight: bold;
+  color: white;
+  background-color: #776b5d;
+  box-sizing: border-box;
+  vertical-align: bottom;
+  margin-right: 5px;
 `;
 
 const Btn2 = styled.button`
-    width: 90px;
-    height: 22px;
-    border-radius: 10px;
-    font-size: 12px;
-    font-weight: bold;
-    color: white;
-    background-color: #776B5D;
-    box-sizing: border-box;
-    vertical-align: bottom;
-    margin-left: 5px;
+  width: 90px;
+  height: 22px;
+  border-radius: 10px;
+  font-size: 12px;
+  font-weight: bold;
+  color: white;
+  background-color: #776b5d;
+  box-sizing: border-box;
+  vertical-align: bottom;
+  margin-left: 5px;
 `;
 
 const Petprofile = () => {
-    const [modalOpen, setModalOpen] = useState(false);
-    const [petName, setPetName] = useState("");
-    const [petGender, setPetGender] = useState("");
-    const [petAge, setPetAge] = useState("");
-    const [petType, setPetType] = useState("");
-    const [petSign, setPetSign] = useState("");
-    const [petImg, setPetImg] = useState("");
+  const [modalOpen, setModalOpen] = useState(false);
+  const [petName, setPetName] = useState("");
+  const [petGender, setPetGender] = useState("");
+  const [petAge, setPetAge] = useState("");
+  const [petType, setPetType] = useState("");
+  const [petSign, setPetSign] = useState("");
+  const [petImg, setPetImg] = useState("");
 
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
-        const closeModal = () => {
-            setModalOpen(false);
-        };
+  const openClick = (name, gender, age, type, sign, img) => {
+    setModalOpen(true);
+    setPetName(name);
+    setPetAge(age);
+    setPetGender(gender);
+    setPetSign(sign);
+    setPetType(type);
+    setPetImg(img);
+  };
 
-        const openClick =(name, gender, age, type, sign, img)=>{
-          setModalOpen(true);
-          setPetName(name);
-          setPetAge(age);
-          setPetGender(gender);
-          setPetSign(sign);
-          setPetType(type);
-          setPetImg(img);
-        };
-  
-
-    const pet = [
-        {
-            image : "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KakaoTalk_20231205_200540280.jpg?alt=media&token=dfcfa49a-1af5-4a43-b196-8a1086d62f20",
-            name : "팡이",
-            gender : "여",
-            age : "11살",
-            type : "진돗개",
-            sign : "겁이 많음, 예쁘고 귀여움"
-        },
-        {
-            image : "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KakaoTalk_20231206_160745815.jpg?alt=media&token=c131d391-d1cb-48d4-93f8-7124247200a3",
-            name : "순돌이",
-            gender : "남",
-            age : "9살",
-            type : "진돗개",
-            sign : "순함, 꼬리가 귀여움, 목욕할때 안도망감"
-        },
-        {
-            image : "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KakaoTalk_20231205_195836703_03.jpg?alt=media&token=ca122b86-bd5d-44c8-85d4-d48351c61a20",
-            name : "멍순이",
-            gender : "여",
-            age : "7살",
-            type : "믹스견",
-            sign : "멋지고 귀여움"
-        },
-        {
-            image : "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KakaoTalk_20231206_160911844_03.jpg?alt=media&token=b582917a-d118-478f-9080-a7ffd512d2d6",
-            name : "우동이",
-            gender : "여",
-            age : "3살",
-            type : "먼치킨",
-            sign : "점프를 잘함, 가끔 토끼가 됨"
-        },
-    ]
-
+  const pet = [
+    {
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KakaoTalk_20231205_200540280.jpg?alt=media&token=dfcfa49a-1af5-4a43-b196-8a1086d62f20",
+      name: "팡이",
+      gender: "여",
+      age: "11살",
+      type: "진돗개",
+      sign: "겁이 많음, 예쁘고 귀여움",
+    },
+    {
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KakaoTalk_20231206_160745815.jpg?alt=media&token=c131d391-d1cb-48d4-93f8-7124247200a3",
+      name: "순돌이",
+      gender: "남",
+      age: "9살",
+      type: "진돗개",
+      sign: "순함, 꼬리가 귀여움, 목욕할때 안도망감",
+    },
+    {
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KakaoTalk_20231205_195836703_03.jpg?alt=media&token=ca122b86-bd5d-44c8-85d4-d48351c61a20",
+      name: "멍순이",
+      gender: "여",
+      age: "7살",
+      type: "믹스견",
+      sign: "멋지고 귀여움",
+    },
+    {
+      image:
+        "https://firebasestorage.googleapis.com/v0/b/dogcat-42fca.appspot.com/o/KakaoTalk_20231206_160911844_03.jpg?alt=media&token=b582917a-d118-478f-9080-a7ffd512d2d6",
+      name: "우동이",
+      gender: "여",
+      age: "3살",
+      type: "먼치킨",
+      sign: "점프를 잘함, 가끔 토끼가 됨",
+    },
+  ];
 
   return (
-        <div style={{marginLeft:'1rem'}}>
-            <BoxTitle>PET PROFILE</BoxTitle>
-            <BoxContent>
-            {pet.map(pet => (
-                <BoxContent1>
-                    <PetProfile src={pet.image}/>
-                    <PetInfo1>
-                        <img src={dogfoot} alt="Dog Foot" className="DogFootImage" />
-                            <PetInfo2>
-                                <div className="PetSign">이름 : {pet.name}</div>
-                                <div className="PetSign">성별 : {pet.gender}</div>
-                                <div className="PetSign">나이 : {pet.age}</div>
-                                <div className="PetSign">종 : {pet.type}<Btn2>도감 보기</Btn2></div>
-                                <div className="PetSign">특이사항 : {pet.sign}</div>
-                            </PetInfo2>
-                        <PetInfo3>
-                            <Btn onClick={()=>openClick(pet.name, pet.gender, pet.age, pet.type, pet.sign, pet.image)}>수정</Btn>
-                        </PetInfo3>
-                    </PetInfo1>
-                </BoxContent1>
-                ))}
-                <BoxContent1>
-                <Footimg style={{width:'220px', height:'220px', marginLeft:'20px'}}/>
-                    <PetInfo1>
-                        <img src={dogfoot} alt="Dog Foot" className="DogFootImage" />
-                            <PetInfo2>
-                                <div className="PetSign">더 많은 친구들을 추가해보세요!</div>
-                            </PetInfo2>
-                        <PetInfo3>
-                            <Btn onClick={()=>openClick()}>추가</Btn>
-                        </PetInfo3>
-                    </PetInfo1>
-                </BoxContent1>
-            </BoxContent>
-            <Petmodal type={1} open={modalOpen} close={closeModal} name={petName} gender={petGender} age={petAge} Type={petType} img={petImg} sign={petSign}>
-            </Petmodal>
-        </div>
-
+    <div style={{ marginLeft: "1rem" }}>
+      <BoxTitle>PET PROFILE</BoxTitle>
+      <BoxContent>
+        {pet.map((pet) => (
+          <BoxContent1>
+            <PetProfile src={pet.image} />
+            <PetInfo1>
+              <img src={dogfoot} alt="Dog Foot" className="DogFootImage" />
+              <PetInfo2>
+                <div className="PetSign">이름 : {pet.name}</div>
+                <div className="PetSign">성별 : {pet.gender}</div>
+                <div className="PetSign">나이 : {pet.age}</div>
+                <div className="PetSign">
+                  종 : {pet.type}
+                  <Btn2>도감 보기</Btn2>
+                </div>
+                <div className="PetSign">특이사항 : {pet.sign}</div>
+              </PetInfo2>
+              <PetInfo3>
+                <Btn
+                  onClick={() =>
+                    openClick(
+                      pet.name,
+                      pet.gender,
+                      pet.age,
+                      pet.type,
+                      pet.sign,
+                      pet.image
+                    )
+                  }
+                >
+                  수정
+                </Btn>
+              </PetInfo3>
+            </PetInfo1>
+          </BoxContent1>
+        ))}
+        <BoxContent1>
+          <Footimg
+            style={{ width: "220px", height: "220px", marginLeft: "20px" }}
+          />
+          <PetInfo1>
+            <img src={dogfoot} alt="Dog Foot" className="DogFootImage" />
+            <PetInfo2>
+              <div className="PetSign">더 많은 친구들을 추가해보세요!</div>
+            </PetInfo2>
+            <PetInfo3>
+              <Btn onClick={() => openClick()}>추가</Btn>
+            </PetInfo3>
+          </PetInfo1>
+        </BoxContent1>
+      </BoxContent>
+      <Petmodal
+        type={1}
+        open={modalOpen}
+        close={closeModal}
+        name={petName}
+        gender={petGender}
+        age={petAge}
+        Type={petType}
+        img={petImg}
+        sign={petSign}
+      ></Petmodal>
+    </div>
   );
 };
 export default Petprofile;
