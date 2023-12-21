880a87825477c8e46d37cab817915ab7f18da62e
incheons <sonincheon94@naver.com>
2023-12-21 15:01:51 +0900

    2023/12/21 손인천 고객센터 jwt수정

--- src/api/ServiceApi.jsx
+++ src/api/ServiceApi.jsx
@@ -1,5 +1,5 @@
-import axios from "axios";
 import Common from "../utill/Common";
+import AxiosInstance from "../utill/AxiosInstance";
 const MUNG_HOST = "http://localhost:8111";
 
 const ServiceApi = {
@@ -7,7 +7,6 @@ const ServiceApi = {
   boardPlus: async (boardType, comment, boardImg, regData) => {
     const res = await Common.TakenToken();
     const email = res.data;
-    const accessToken = Common.getAccessToken();
     const serviceData = {
       memberEmail: email,
       boardType: boardType,
@@ -16,59 +15,33 @@ const ServiceApi = {
       regData: regData,
     };
     console.log(serviceData);
-
-    return await axios.post(MUNG_HOST + "/post/new", serviceData, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-      },
-    });
+    return await AxiosInstance.post(MUNG_HOST + "/post/new", serviceData);
   },
   // 회원별 조회
   oneBoardByMemberEmail: async () => {
     const res = await Common.TakenToken();
     const email = res.data;
-    const accessToken = Common.getAccessToken();
-    return await axios.get(MUNG_HOST + `/post/list/${email}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-      },
-    });
+    return await AxiosInstance.get(MUNG_HOST + `/post/list/${email}`);
   },
   // 문의 수정
   boardUp: async (id, boardType, comment, boardImg) => {
     console.log(id, boardType, comment, boardImg);
     const res = await Common.TakenToken();
     const email = res.data;
-    const accessToken = Common.getAccessToken();
     const boardUpData = {
       memberEmail: email,
       boardType: boardType,
       comment: comment,
       boardImg: boardImg,
     };
-    return await axios.put(
+    return await AxiosInstance.put(
       Common.MUNG_HOST + `/post/modify/${id}`,
-      boardUpData,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: "Bearer " + accessToken,
-        },
-      }
-    );
+      boardUpData);
   },
   // 문의 삭제
   boardDel: async (id) => {
-    const accessToken = Common.getAccessToken();
     console.log(id);
-    return await axios.delete(MUNG_HOST + `/post/delete/${id}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer " + accessToken,
-      },
-    });
+    return await AxiosInstance.delete(MUNG_HOST + `/post/delete/${id}`);
   },
 };
 export default ServiceApi;
